ba5d3284af96149c7dae25a8e1ac154e79b41b21
Noelia Ruiz <ruiz.noeliaromina@gmail.com>
2024-04-05 02:11:21 -0300

    Lanzamiento de servidor, rutas y manejo de errores

--- src/app.js
+++ src/app.js
@@ -4,14 +4,10 @@ import morgan from "morgan";
 const app = express(); //para lanzar de express 
 
 app.use(morgan("dev")); //morgan en modo dev, nos dá por consola mensajes mas cortos y limpios
+app.use(express.json()); //convertimos lo que llega desde el front a objeto de javascript
+app.use(express.urlencoded({extended: false})); //para codificar formularios html CORTOS
 
 app.get("/", (req, res) => res.json({ message: "Bienvenidos a nuestro proyecto PERN" }));
-
-//generamos otra ruta
-app.get("/test", (req, res) => {
-    throw new Error('Error generado por el usuario');
-    res.send('test');
-});
 
 //Creamos un manejador de errores
 app.use((err, req, res, next) =>{
