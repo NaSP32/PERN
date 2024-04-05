cccd0eef39e9375165a72d57be3b7256d3e23619
Noelia Ruiz <ruiz.noeliaromina@gmail.com>
2024-04-05 02:29:43 -0300

    Creación de rutas para el backend

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express from "express"; //modulo ya instalado
 import morgan from "morgan";
+import tareasRoutes from "./router/tareas.routes.js"
 
 const app = express(); //para lanzar de express 
 
+//Middlewares- Convierte a objetos de JS lo que llega al backend
 app.use(morgan("dev")); //morgan en modo dev, nos dá por consola mensajes mas cortos y limpios
 app.use(express.json()); //convertimos lo que llega desde el front a objeto de javascript
 app.use(express.urlencoded({extended: false})); //para codificar formularios html CORTOS
 
 app.get("/", (req, res) => res.json({ message: "Bienvenidos a nuestro proyecto PERN" }));
+app.use("/tareas", tareasRoutes);
 
 //Creamos un manejador de errores
 app.use((err, req, res, next) =>{
