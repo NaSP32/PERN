6bf3b41358d55d070be6ba57704cd1acf6cc02ea
Noelia Ruiz <ruiz.noeliaromina@gmail.com>
2024-04-05 01:35:26 -0300

    Se lanza servidor express y manejo de error 505

--- src/app.js
+++ src/app.js
@@ -5,5 +5,19 @@ const app = express(); //para lanzar de express
 
 app.use(morgan("dev")); //morgan en modo dev, nos dá por consola mensajes mas cortos y limpios
 
-app.get("/", (req, res) => res.json({message: "Bienvenidos a nuestro proyecto PERN"}));
+app.get("/", (req, res) => res.json({ message: "Bienvenidos a nuestro proyecto PERN" }));
+
+//generamos otra ruta
+app.get("/test", (req, res) => {
+    throw new Error('Error generado por el usuario');
+    res.send('test');
+});
+
+//Creamos un manejador de errores
+app.use((err, req, res, next) =>{
+    res.status(500).json({
+        status:"error",
+        message: err.message
+    }); //500 es un error crìtico a nivel global al fallar alguna de las rutas
+});
 export default app;
